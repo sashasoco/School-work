4089b9a20b73f1fd21339cca1521134692e459f0
sasha <21ASocolovsky.ACC@lwetb.ie>
2025-09-02 20:22:05 +0100

    Delete PG 174 Q8.py

--- PG 174 Q8.py
+++
@@ -1,8 +0,0 @@
-#Alexander Socolovsky
-#02 September 2025
-#Pg 174 Q8
-CM=float(input("Give me your height"))
-Inch=float(CM*2.54)
-Feet=float(Inch/12)
-Inch_Rem= Feet%12
-print("Your height is",Feet,"feet and",Inch_Rem,"inches")

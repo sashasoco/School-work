501da3cfe3d9e2d4297a4413d1574f824849ec23
sasha <21ASocolovsky.ACC@lwetb.ie>
2025-09-05 17:42:55 +0100

    Delete Question 7.py

--- Question 7.py
+++
@@ -1,9 +0,0 @@
-#Alexander Socolovsky
-#01 September 2025
-#Question 7
-Price=input("How much was this?")
-Price=int(Price)
-People=input("And how many people are here?")
-People=int(People)
-Split_Pay=Price/People
-print("We have to pay",Split_Pay,"each")

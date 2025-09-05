44538d425ccc834ba2c2ace8f95a4e864b37fb28
sasha <21ASocolovsky.ACC@lwetb.ie>
2025-09-05 09:43:57 +0100

    Add files via upload

---
+++ Assignment2 Q4.py
@@ -0,0 +1,8 @@
+#Alexander Socolovsky
+#09/05/2025
+#Assignment 2 Q4
+Colour=input("Whats your favourite colour")
+if(Colour=="red"or Colour=="RED"or Colour=="Red"):
+    print("I like red too")
+else:
+    print("I dont like the colour",Colour,"I prefer red")

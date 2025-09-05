7f9eab40f7287525ef394a86e62c1ed82bfbff49
sasha <21ASocolovsky.ACC@lwetb.ie>
2025-09-05 17:43:10 +0100

    Delete Question 5.py

--- Question 5.py
+++
@@ -1,9 +0,0 @@
-#Alexander Socolovsky
-#01 September 2025
-#Question 5
-OG_pizza=input("How many pizza slices were there originally")
-OG_pizza=int(OG_pizza)
-pizza_ate=input("How many did you eat")
-pizza_ate=int(pizza_ate)
-pizza_left=OG_pizza-pizza_ate
-print("There are",pizza_left,"slices left")

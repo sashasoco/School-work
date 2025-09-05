56b039ed250ab60389cfa112fe5a8e2b8fbaeaa9
sasha <21ASocolovsky.ACC@lwetb.ie>
2025-09-05 17:43:04 +0100

    Delete Question 6.py

--- Question 6.py
+++
@@ -1,8 +0,0 @@
-#Alexander Socolovsky
-#01 September 2025
-#Question 6
-Name=input("what's your name?")
-Age=input("What age are you?")
-Age=int(Age)
-New_Age=Age+1
-print("Hello",Name,"next year you will be",New_Age,"years old.")

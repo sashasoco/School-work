1e341a15af004893338049cf9a26960aa817d896
sasha <21ASocolovsky.ACC@lwetb.ie>
2025-09-05 17:38:56 +0100

    Delete PG 174 Q10.py

--- PG 174 Q10.py
+++
@@ -1,7 +0,0 @@
-#Alexander Socolovsky
-#02 September 2025
-#Pg 174 Q10
-Base=int(input("Give me a base of a triangle"))
-Height=int(input("Now give me the height"))
-Area=Base*Height*0.5
-print("The area is",Area,)

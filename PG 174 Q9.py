f56978f3b4a9e787c9b0e8c7c402fd56d02037e9
sasha <21ASocolovsky.ACC@lwetb.ie>
2025-09-05 17:41:56 +0100

    Delete PG 174 Q9.py

--- PG 174 Q9.py
+++
@@ -1,8 +0,0 @@
-#Alexander Socolovsky
-#02 September 2025
-#Pg 174 Q9
-Num1=int(input("Give me a number"))
-Num2=int(Num1*Num1)
-Num3=int(Num2*Num1)
-Num4=int(Num3*Num1)
-print("Your number squared is",Num2,"and your number cubed is",Num3,"and your number to the power of four is",Num4)

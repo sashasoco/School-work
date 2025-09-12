9456770d55015599bece06082b2590ce936e217e
sasha <21ASocolovsky.ACC@lwetb.ie>
2025-09-12 08:50:02 +0100

    Add files via upload

---
+++ Assignment 4/Assignment 4 Q5.py
@@ -0,0 +1,10 @@
+#Alexander Socolovsky
+#09/09/2025
+#Programming 4 Q5
+MultiplyNum=1
+Num=int(input("Give a number"))
+for i in range(12):
+    Answer=Num*MultiplyNum
+    print(Num,"*",MultiplyNum,"=",Answer)
+    MultiplyNum+=1
+ 
---
+++ Assignment 4/Assignment 4 Q6.py
@@ -0,0 +1,11 @@
+#Alexander Socolovsky
+#09/09/2025
+#Programming 4 Q4
+Digits=0
+Num=(input("Give me a number"))
+for i in Num:
+    Digits+=1
+print(Digits)
+    
+    
+    
